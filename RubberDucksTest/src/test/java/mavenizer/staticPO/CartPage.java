7e87cab79bf42f94e801699dd45e59b794169729
Nastya Vasilenya <vasileniaanastasiya@gmail.com>
2022-09-28 19:50:09 +0300

    Empty bin testcase added

--- RubberDucksTest/src/test/java/mavenizer/staticPO/CartPage.java
+++ RubberDucksTest/src/test/java/mavenizer/staticPO/CartPage.java
@@ -1,4 +1,11 @@
 package mavenizer.staticPO;
 
+import mavenizer.helpers.LocatorHelper;
+import org.openqa.selenium.WebDriver;
+
 public class CartPage {
+    public static String getEmptyBinMessage(WebDriver driver) {
+        return driver.findElement(LocatorHelper.getLocator("CartPage.emptyBinMessage")).getText();
+    }
+
 }
